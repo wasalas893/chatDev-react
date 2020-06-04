ced0aaa6041e7b0f5cf8bab50017d0c2e635d8b3
wasalas <wasalas893@gmail.com>
2020-06-04 11:11:37 +0530

    create the login register components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       ggggffgggg
+      <div >
+       App
       </div>
     );
   }
