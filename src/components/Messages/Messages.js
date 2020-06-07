063f31f8c5d3ad655924b4f4fdd1d29c218d20a4
wasalas <wasalas893@gmail.com>
2020-06-07 11:47:15 +0530

    send the massage data from firebase

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import { Segment,Comment } from 'semantic-ui-react';
+import firebase from '../../firebase';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 
 class Messages extends React.Component{
+      state={
+          messagesRef:firebase.database().ref('messages'),
+          channel:this.props.currentChannel,
+          user:this.props.currentUser
+      }
     render(){
+        const { messagesRef,channel,user }=this.state;
         return(
             <React.Fragment>
                 <MessagesHeader />
@@ -17,7 +24,9 @@ class Messages extends React.Component{
                 </Segment>
 
 
-                <MessageForm />
+                <MessageForm 
+                messagesRef={messagesRef} currentChannel={channel}
+                currentUser={user} />
             </React.Fragment>
             
         );
