48f1c646f68b2820940890b4bc0e8359eebf746c
wasalas <wasalas893@gmail.com>
2020-06-10 08:57:08 +0530

    create massage show progress bar

--- src/components/Messages/ProgressBar.js
+++ src/components/Messages/ProgressBar.js
@@ -3,7 +3,7 @@ import { Progress  } from 'semantic-ui-react';
 
 const ProgressBar=({ uploadState,percentUploaded })=> (
 
-    uploadState &&(
+    uploadState  &&(
         <Progress
         className="progress_bar"
         percent={percentUploaded}
